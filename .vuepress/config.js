800bf4a4f15b8c1ac0951e680c465751786fb494
sunrabbit123 <qudwls185@naver.com>
2021-06-14 07:23:58 +0900

    add vuepress config

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: "Sunrabbit's TIL",
+    description: "GSM Service 모음 사이트",
+    dest: "build",
+    base: "/TIL/",
+    head: [["link", { rel: "icon", href: "/logo.png" }]],
+    themeConfig: {
+      sidebar: [
+        "/",
+      ],
+      nav: [
+        {
+          text: "GitHub",
+          link: "https://github.com/sunrabbit123/GSM-Service/tree/main",
+        },
+      ],
+    },
+  };
