90385785b16a7dddae5a917e06d4b479598bfbcd
sunrabbit123 <qudwls185@naver.com>
2021-07-01 21:37:31 +0900

    move docs

--- .vuepress/components/comment.vue
+++
@@ -1,8 +0,0 @@
-<script src="https://utteranc.es/client.js"
-        repo="sunrabbit123/GSM-Service"
-        issue-term="title"
-        label="Comment"
-        theme="github-light"
-        crossorigin="anonymous"
-        async>
-</script>
