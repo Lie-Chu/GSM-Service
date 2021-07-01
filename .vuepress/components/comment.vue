ef19736d04f04623cc7388ffd56e27458a1d9d72
sunrabbit123 <qudwls185@naver.com>
2021-07-01 20:52:00 +0900

    fix : Comment

--- .vuepress/components/comment.vue
+++ .vuepress/components/comment.vue
@@ -1,5 +1,8 @@
 <script src="https://utteranc.es/client.js"
         repo="sunrabbit123/GSM-Service"
-        issue-term="pathname"
+        issue-term="title"
+        label="Comment"
+        theme="github-light"
+        crossorigin="anonymous"
         async>
 </script>
