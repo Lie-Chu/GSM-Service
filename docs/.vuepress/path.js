19392dd78e6bfb3f44253b5e96d697c42ad61684
sunrabbit123 <qudwls185@naver.com>
2021-07-01 23:12:55 +0900

    add : gogoBot

--- docs/.vuepress/path.js
+++ docs/.vuepress/path.js
@@ -2,4 +2,4 @@ exports.WebList = []; // 웹 사이트
 exports.AppList = ["app/bobnaenwa"]; // 모바일, 데스크탑 앱
 exports.LibList = ["lib/schoolInfo", "lib/school-info"]; // 라이브러리 또는 패키지
 exports.EmbList = []; // 임베디드 
-exports.BotList = ["bot/shaki"]; // 봇
+exports.BotList = ["bot/shaki", "bot/gogo"]; // 봇
