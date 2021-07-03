8315333c0cbbfa2583f74416d6a94ef3debdd606
sunrabbit123 <qudwls185@naver.com>
2021-07-04 00:38:32 +0900

    add : footer

---
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="global-layout">
+    <component :is="$page.path ? 'Layout' : 'NotFound'" />
+    <footer class="siteFooter">
+      <div class="container">
+        <p class="siteFooterContact">
+          qudwls185@naver.com .
+          +82-10-5185-8462 .
+          <a href="https://github.com/sunrabbit123" target="_blank">GitHub</a> .
+          <a href="https://github.com/sunrabbit123/GSM-Service/" target="_blank">Repository</a>
+        </p>
+    </div>
+    </footer>
+  </div>
+</template>
