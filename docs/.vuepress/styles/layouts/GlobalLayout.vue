cce89c1dfd2843b2aa5dadcbbea178dafc69059c
sunrabbit123 <qudwls185@naver.com>
2021-07-03 23:39:53 +0900

    add : font

---
+++ docs/.vuepress/styles/layouts/GlobalLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="global-layout">
+    <component :is="$page.path ? 'Layout' : 'NotFound'" />
+    <footer class="siteFooter">
+      &copy; 개발자 오병진
+    </footer>
+  </div>
+</template>
