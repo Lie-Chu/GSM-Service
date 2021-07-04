c8a73de851d3f62c5dac9da7008990c82e949c61
sunrabbit123 <qudwls185@naver.com>
2021-07-04 22:34:39 +0900

    update: Footer

--- docs/.vuepress/styles/layouts/GlobalLayout.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="global-layout">
-    <component :is="$page.path ? 'Layout' : 'NotFound'" />
-    <footer class="siteFooter">
-      &copy; 개발자 오병진
-    </footer>
-  </div>
-</template>
