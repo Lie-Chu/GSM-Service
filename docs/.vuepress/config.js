abed64e66ef0b67d1c38b812b248ed1291922f80
sunrabbit123 <qudwls185@naver.com>
2021-07-05 10:40:54 +0900

    update: sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const PATH = require("./path");
+const sidebar = require("./path");
 
 module.exports = {
   title: "GSM-Service",
@@ -8,29 +8,7 @@ module.exports = {
   head: [["link", { rel: "icon", href: "/logo.png" }]],
   themeConfig: {
     logo : "https://user-images.githubusercontent.com/64676070/124141211-30acac80-dac4-11eb-93e3-d4d4165f1b74.png",
-    sidebar: [
-      "./",
-      {
-        title: "웹 서비스",
-        children : PATH.WebList
-      },
-      {
-        title : "앱 서비스",
-        children : PATH.AppList
-      },
-      {
-        title : "라이브러리",
-        children : PATH.LibList,
-      },
-      {
-        title : "임베디드",
-        children : PATH.EmbList
-      },
-      {
-        title: "디스코드 봇",
-        children : PATH.BotList
-      },
-    ],
+    sidebar,
     nav: [
       {
         text: "GitHub",
--- docs/.vuepress/path.js
+++ docs/.vuepress/path.js
@@ -1,5 +1,55 @@
-exports.WebList = []; // 웹 사이트
-exports.AppList = ["app/bobnaenwa"]; // 모바일, 데스크탑 앱
-exports.LibList = ["lib/schoolInfo", "lib/school-info"]; // 라이브러리 또는 패키지
-exports.EmbList = []; // 임베디드 
-exports.BotList = ["bot/shaki", "bot/gogo"]; // 봇
+const WebList = ["WhatIsGit"]; // 웹 사이트
+const LibList = ["schoolInfo", "school-info"]; // 라이브러리 또는 패키지
+const DesktopAppList = ["bobnaenwa"]; // 데스크탑 앱
+const AndoroidAppList = []; // 안드로이드 앱
+const IosAppList = []; // IOS 앱
+const EmbList = []; // 임베디드 
+const BotList = ["shaki", "gogo"]; // 봇
+
+const getAPP = (v) => { return `app/${v}/`};
+
+module.exports = [
+    {
+      title: "Main", path: '/', sidebarDepth: 2,
+    },
+    {
+      title: "웹 서비스",
+      children : WebList.map(f => `web/${f}`),
+      sidebarDepth: 2
+    },
+    {
+      title : "앱 서비스",
+      children : [
+          {
+              title : "데스크탑 앱",
+              children : DesktopAppList.map(f => `${getAPP("Desktop")}${f}`),
+              sidebarDepth: 2
+          },
+          {
+              title : "안드로이드 앱",
+              children : AndoroidAppList.map(f => `${getAPP("Android")}${f}`),
+              sidebarDepth: 2
+          },
+          {
+              title : "IOS 앱",
+              children : IosAppList.map(f => `${getAPP("ios")}${f}`),
+              sidebarDepth: 2
+          }
+      ]
+    },
+    {
+      title : "라이브러리",
+      children : LibList.map(f => `lib/${f}`),
+      sidebarDepth: 2
+    },
+    {
+      title : "임베디드",
+      children : EmbList.map(f => `emb/${f}`),
+      sidebarDepth: 2
+    },
+    {
+      title: "디스코드 봇",
+      children : BotList.map(f => `bot/${f}`),
+      sidebarDepth: 2
+    },
+]
